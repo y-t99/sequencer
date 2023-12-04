4ab994e6cd703ef11e430f3b4804ad6b06450170
Itou Ng <13060889537@sina.cn>
2023-12-04 22:49:00 +0800

    feat: test & time & mod tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::time::{SystemTime, UNIX_EPOCH};
+
+const ALPHABET: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+pub fn next() -> String {
+    let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("system time before Unix epoch");
+    let seconds_since_epoch = now.as_secs();
+    let years_since_epoch = seconds_since_epoch / (365 * 24 * 60 * 60) + 1970;
+    years_since_epoch.to_string()
 }
 
 #[cfg(test)]
@@ -7,8 +14,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn next_sequence() {
+        let sequence = next();
+        assert_eq!(sequence, "2023");
     }
 }
